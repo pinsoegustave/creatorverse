294707d834d541aa6c65241aa5bb270cdf6462c8
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-05-12 20:43:06 -0400

    add edit creator form

--- src/pages/EditCreator.tsx
+++ src/pages/EditCreator.tsx
@@ -1,7 +1,19 @@
-// import React from 'react'
+import CreatorForm from '../components/CreatorForm'
 
 export default function EditCreator() {
-  return (
-    <div>EditCreator</div>
-  )
+  const existingCreator = {
+    name: 'John Doe',
+    description: 'Amazing creator...',
+    youtube: 'https://youtube.com/@johndoe',
+    instagram: 'https://instagram.com/johndoe',
+    x: 'https://x.com/johndoe',
+    imageUrl: 'https://example.com/image.jpg',
+  }
+
+  const handleSubmit = (values: any) => {
+    console.log('Updated creator:', values)
+    // update in database here
+  }
+
+  return <CreatorForm initialValues={existingCreator} onSubmit={handleSubmit} />
 }
